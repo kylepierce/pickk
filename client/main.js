fc20f6299d71011340297e73de40d7b0479bfa2e
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2015-05-16 16:53:05 -0400

    Changed how user data is collected

--- client/main.js
+++ client/main.js
@@ -25,68 +25,75 @@ Template.questionCard.helpers({
 });
 
 Template.questionCard.events({
-	'click [data-action=value100]' : function(event, template){
+
+	//Session that saves users data until they hit submit.
+
+	// Session.set({
+	// 	questionAnswered: this._id,
+	// 	optionChosen: ,
+	// 	wager: 
+	// });
+
+	//Function that will store wager into a session
+
+	'submit form': function(event) {
 		event.preventDefault();
-
-		$('button').not(this).removeClass('button-energized');
-		$(event.target).toggleClass("button-energized");
-
-	},
-	'click [data-action=value500]' : function(event, template){
-		event.preventDefault();
-
-		$('button').not(this).removeClass('button-energized');
-		$(event.target).toggleClass("button-energized");
-
-	},
-	'click [data-action=value1000]' : function(event, template){
-		event.preventDefault();
-
-		$('button').not(this).removeClass('button-energized');
-		$(event.target).toggleClass("button-energized");
-
-	},
-
-	'click [data-action=answerRun]': function(){
-		event.preventDefault(); 
-		var questionId = this._id;
+		var questionId = this._id
 		var currentUser = Meteor.userId();
-
-		Session.set('answered', questionId);
-		var answered = Session.get('answered');
-		console.log(currentUser + " Answered Yes to question " + questionId);
-
-		Meteor.call('questionAnswered', currentUser, answered, "run", 100);
-
-	},
-	'click [data-action=answerPass]': function(){
-		event.preventDefault();
-		var questionId = this._id;
-		var currentUser = Meteor.userId();
-
-		Session.set('answered', questionId);
-		var answered = Session.get('answered');
-
-		Meteor.call('questionAnswered', currentUser, answered, "pass", 100);
-	},
-	'click [data-action=answerFumble]': function(){
-		event.preventDefault();
-		var questionId = this._id;
-		var currentUser = Meteor.userId();
-
-		Session.set('answered', questionId);
-		var answered = Session.get('answered');
-
-		Meteor.call('questionAnswered', currentUser, answered, "fumble", 100);
-	},
-	'click [data-action=answerInterception]': function(){
-		event.preventDefault();
-		var questionId = this._id;
-		var currentUser = Meteor.userId();
-
-		Session.set('answered', questionId);
-		var answered = Session.get('answered');
-
-		Meteor.call('questionAnswered', currentUser, answered, "interception", 100);
+		var answer = event.target.play.value;
+		var wager = event.target.wager.value;
+		console.log('User: ' + currentUser + ' answered question ' + questionId + ' -- ' + answer + ' ' + wager);
 	}
+
+	//Function that stores option chosen to the session.
+
+
+
+	//Submit the session to the database
+	// 'submit form': function(){
+	// 	console.log("Submitted!");
+	// },
+
+	// 'click [data-action=answerRun]': function(){
+	// 	event.preventDefault(); 
+	// 	var questionId = this._id;
+	// 	var currentUser = Meteor.userId();
+
+	// 	Session.set('answered', questionId);
+	// 	var answered = Session.get('answered');
+	// 	console.log(currentUser + " Answered Yes to question " + questionId);
+
+	// 	Meteor.call('questionAnswered', currentUser, answered, "run", 100);
+
+	// },
+	// 'click [data-action=answerPass]': function(){
+	// 	event.preventDefault();
+	// 	var questionId = this._id;
+	// 	var currentUser = Meteor.userId();
+
+	// 	Session.set('answered', questionId);
+	// 	var answered = Session.get('answered');
+
+	// 	Meteor.call('questionAnswered', currentUser, answered, "pass", 100);
+	// },
+	// 'click [data-action=answerFumble]': function(){
+	// 	event.preventDefault();
+	// 	var questionId = this._id;
+	// 	var currentUser = Meteor.userId();
+
+	// 	Session.set('answered', questionId);
+	// 	var answered = Session.get('answered');
+
+	// 	Meteor.call('questionAnswered', currentUser, answered, "fumble", 100);
+	// },
+	// 'click [data-action=answerInterception]': function(){
+	// 	event.preventDefault();
+	// 	var questionId = this._id;
+	// 	var currentUser = Meteor.userId();
+
+	// 	Session.set('answered', questionId);
+	// 	var answered = Session.get('answered');
+
+	// 	Meteor.call('questionAnswered', currentUser, answered, "interception", 100);
+	// }
 });
