bb62a93dea3a90349657643dffd9f9b4c6e49d04
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2015-06-19 14:45:30 -0400

    Submit button is inactive until play and wagers are clicked

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -6,10 +6,12 @@ Meteor.subscribe('userAnswer');
 
 
 // this should be changed to startup. There might also be some additions for user types
-Template.home.rendered = function () {
+Template.home.rendered = function (template) {
   if (!Meteor.loggingIn() && !Meteor.user()) {
    	 Router.go('/landing');
   }
+  $("#submit-response").prop("disabled", true);
+  $("#submit-response").addClass("button-assertive");
 };
 
 Template.questionCard.helpers({
@@ -26,6 +28,12 @@ Template.questionCard.helpers({
 });
 
 Template.questionCard.events({
+	'click input:radio[name=wager]':function(event, template) {
+		wager = template.find('input:radio[name=wager]:checked').value
+		$("#submit-response").prop("disabled", false)
+		$("#submit-response").addClass('button-balanced');
+	},
+
 	'submit form': function(event, template) {
 		event.preventDefault();
 		var questionId = this._id;
