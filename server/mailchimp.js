d903445a029eacd2d90e26d0d4106e01691e558b
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-05-27 15:58:29 +0300

    Add Facebook email

--- server/mailchimp.js
+++ server/mailchimp.js
@@ -1,13 +1,19 @@
 mailChimpLists = new MailChimpLists(Meteor.settings.private.mailchimp.apiKey);
 
 mailChimpLists.subscribeUser = function(user, defaults, callback) {
-  if (!(user.emails && user.emails.length)) {
+  var email;
+  if (user.emails && user.emails.length && user.emails[0].address) {
+    email = user.emails[0].address;
+  } else if (user.services && user.services.facebook && user.services.facebook.email) {
+    email = user.services.facebook.email;
+  }
+  if (!email) {
     return; // Twitter login
   }
   return this.subscribe(_.defaults({
     id: Meteor.settings.private.mailchimp.listId,
     email: {
-      email: user.emails[0].address
+      email: email
     },
     merge_vars: {
       FNAME: user.profile.firstName,
