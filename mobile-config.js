210b50451c4606c8b74c8fb89fcbf0b7c244fa28
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2016-06-06 02:24:10 -0400

    Updated icon files

--- mobile-config.js
+++ mobile-config.js
@@ -1,61 +1,41 @@
-App.info({
-  name: 'Pickk',
-  description: 'Predict sports events in real time',
-  author: 'Pickk Corporation',
-  email: 'kyle@pickk.co',
-  website: 'http://pickk.co',
-  version: '0.0.6'
-});
-
 App.icons({
-  // iPhone Icons
-  // 'iphone': 'resources/icons/ios/new_iphone.png',
-  'iphone_2x': 'resources/icons/ios/Icon-602x.png',
-  'iphone_3x': 'resources/icons/ios/Icon-603x.png',
-  'ipad_pro': 'resources/icons/ios/Icon-iPadPro2x.png', 
-  'ios_settings': 'resources/icons/ios/Icon-Small.png', 
-  'ios_settings_2x': 'resources/icons/ios/Icon-Small2x.png', 
-  'ios_settings_3x': 'resources/icons/ios/Icon-Small3x.png', 
-  'ios_spotlight': 'resources/icons/ios/Icon-Spotlight-40.png', 
-  'ios_spotlight_2x': 'resources/icons/ios/Icon-Spotlight-402x.png', 
-
-
-  // iPad Icons
-  'ipad': 'resources/icons/ios/new_ipad1x.png',
-  'ipad_2x': 'resources/icons/ios/new_ipad2x.png',
-
-  // Android Phones
-  // 'android_ldpi': 'resources/icons/android/36.png',
-  'android_mdpi': 'resources/icons/android/48.png',
-  'android_hdpi': 'resources/icons/android/72.png',
-  'android_xhdpi': 'resources/icons/android/96.png',
-  // 'android_xxhdpi': (144x144)
-  // 'android_xxxhdpi': (192x192)
+  "iphone_2x": "resources/icons/iphone_2x.png", // 120x120
+  "iphone_3x": "resources/icons/iphone_3x.png", // 180x180
+  "ipad": "resources/icons/ipad.png", // 76x76
+  "ipad_2x": "resources/icons/ipad_2x.png", // 152x152
+  "ipad_pro": "resources/icons/ipad_pro.png", // 167x167
+  "ios_settings": "resources/icons/ios_settings.png", // 29x29
+  "ios_settings_2x": "resources/icons/ios_settings_2x.png", // 58x58
+  "ios_settings_3x": "resources/icons/ios_settings_3x.png", // 87x87
+  "ios_spotlight": "resources/icons/ios_spotlight.png", // 40x40
+  "ios_spotlight_2x": "resources/icons/ios_spotlight_2x.png", // 80x80
+  "android_mdpi": "resources/icons/android_mdpi.png", // 48x48
+  "android_hdpi": "resources/icons/android_hdpi.png", // 72x72
+  "android_xhdpi": "resources/icons/android_xhdpi.png", // 96x96
+  "android_xxhdpi": "resources/icons/android_xxhdpi.png", // 144x144
+  "android_xxxhdpi": "resources/icons/android_xxxhdpi.png" // 192x192
 });
 
 App.launchScreens({
-  // iPhone
-  'iphone_2x': 'resources/splash/iphone_portrait_4_1x.png',
-  'iphone5': 'resources/splash/iphone_portrait_5_1x.png',
-  'iphone6': 'resources/splash/iphone_portrait_6_1x.png',
-  'iphone6p_portrait': 'resources/splash/iphone_portrait_6p_1x.png',
+  "iphone_2x": "resources/splashes/iphone_2x.png", // 640x490
+  "iphone5": "resources/splashes/iphone5.png", // 640x1136
+  "iphone6": "resources/splashes/iphone6.png", // 750x1334
+  "iphone6p_portrait": "resources/splashes/iphone6p_portrait.png", // 2208x1242
+  "iphone6p_landscape": "resources/splashes/iphone6p_landscape.png", // 2208x1242
+  "ipad_portrait": "resources/splashes/ipad_portrait.png", // 768x1024
+  "ipad_portrait_2x": "resources/splashes/ipad_portrait_2x.png", // 1536x2048
+  "ipad_landscape": "resources/splashes/ipad_landscape.png", // 1024x768
+  "ipad_landscape_2x": "resources/splashes/ipad_landscape_2x.png", // 2048x1536
+  "android_mdpi_portrait": "resources/splashes/android_mdpi_portrait.png", // 320x480
+  "android_mdpi_landscape": "resources/splashes/android_mdpi_landscape.png", // 480x320
+  "android_hdpi_portrait": "resources/splashes/android_hdpi_portrait.png", // 480x800
+  "android_hdpi_landscape": "resources/splashes/android_hdpi_landscape.png", // 800x480
+  "android_xhdpi_portrait": "resources/splashes/android_xhdpi_portrait.png", // 720x1280
+  "android_xhdpi_landscape": "resources/splashes/android_xhdpi_landscape.png", // 1280x720
+  "android_xxhdpi_portrait": "resources/splashes/android_xxhdpi_portrait.png", // 1080x1440
+  "android_xxhdpi_landscape": "resources/splashes/android_xxhdpi_landscape.png" // 1440x1080
+})
 
-  //iPad
-  'ipad_portrait': 'resources/splash/ipad_portrait_7-8_1x.png',
-  'ipad_portrait_2x': 'resources/splash/ipad_portrait_7-8_2x.png',
-  'ipad_landscape': 'resources/splash/ipad_landscape_7-8_1x.png',
-  'ipad_landscape_2x': 'resources/splash/ipad_landscape_7-8_2x.png',
-
-  // Android
-  // 'android_ldpi_portrait': 'resources/splash/ldpi-portrait.png',
-  // 'android_ldpi_landscape': 'resources/splash/ldpi-landscape.png',
-  'android_mdpi_portrait': 'resources/splash/mdpi-portrait.png',
-  'android_mdpi_landscape': 'resources/splash/mdpi-landscape.png',
-  'android_hdpi_portrait': 'resources/splash/hdpi-portrait.png',
-  'android_hdpi_landscape': 'resources/splash/hdpi-landscape.png',
-  'android_xhdpi_portrait': 'resources/splash/xhdpi-portrait.png',
-  'android_xhdpi_landscape': 'resources/splash/xhdpi-landscape.png'
-});
 
 App.accessRule('*');
 App.accessRule("blob:*");
