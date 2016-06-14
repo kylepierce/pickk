ec48b41db1f9df2fa692d30099e42fd733d5515e
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-06-14 08:53:12 +0300

    Fix easy search for groups

--- client/templates/groups/_searchGroups.js
+++ client/templates/groups/_searchGroups.js
@@ -1,5 +1,8 @@
 Template.searchGroupBox.helpers({
   GroupsIndex: function() {
     return GroupsIndex;
+  },
+  href: function() {
+    return Router.routes["group.show"].path({_id: this.__originalId})
   }
 })
