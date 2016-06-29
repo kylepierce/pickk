6bbbbfd59cb7e00bada6a8efc17548e31c2800d6
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-06-29 12:39:56 +0300

    Add cleanUsernames, fix the case sensitivity

--- server/migrations.js
+++ server/migrations.js
@@ -1,17 +1,43 @@
 var version = 1;
 
+var cleanUsernames = function() {
+  Meteor._debug("[cleanUsernames] Running");
+  var processedUsernames = [];
+  Meteor.users.find().forEach(function(user) {
+    var oldUsername = user.profile.username;
+    if (!oldUsername) {
+      return;
+    }
+    var newUsername = oldUsername.replace(/[^\w\d_]/g, "");
+    if (oldUsername != newUsername) {
+      Meteor.users.update(user._id, {$set: {"profile.username": newUsername}});
+    }
+    processedUsernames.push(newUsername);
+    if (processedUsernames.length % 100 === 0) {
+      Meteor._debug("[cleanUsernames] Processed " + processedUsernames.length + " usernames");
+    }
+  });
+  Meteor._debug("[cleanUsernames] Done");
+};
+
 var unsetDuplicateUsernames = function() {
   Meteor._debug("[unsetDuplicateUsernames] Running");
   var processedUsernames = [];
   Meteor.users.find().forEach(function(user) {
-    if (!user.profile.username) {
+    var username = user.profile.username;
+    if (!username) {
       return;
     }
-    if (~processedUsernames.indexOf(user.profile.username)) {
+    var lowercasedUsername = username.toLowerCase();
+    if (~processedUsernames.indexOf(lowercasedUsername)) {
       return;
     }
-    processedUsernames.push(user.profile.username);
-    Meteor.users.update({_id: {$ne: user._id}, "profile.username": user.profile.username}, {$unset: {"profile.username": 0}}, {multi: true});
+    processedUsernames.push(lowercasedUsername);
+    var result = Meteor.users.update({_id: {$ne: user._id}, "profile.username": new RegExp("^" + escapeRegExp(username) + "$", "i")}, {$unset: {"profile.username": 0}}, {multi: true});
+    if (result) {
+      Meteor._debug("[unsetDuplicateUsernames] " + username + ": " + result);
+    }
+
     if (processedUsernames.length % 100 === 0) {
       Meteor._debug("[unsetDuplicateUsernames] Processed " + processedUsernames.length + " usernames");
     }
@@ -36,7 +62,7 @@ var setUsernames = function() {
       username = usernames[i].replace(/[^\w\d_]/g, "");
       if (username) {
         Meteor._debug("[setUsernames] #" + user._id + ": trying " + username);
-        if (!Meteor.users.findOne({"profile.username": username})) {
+        if (!Meteor.users.findOne({"profile.username": new RegExp("^" + escapeRegExp(username) + "$", "i")})) {
           Meteor._debug("[setUsernames] #" + user._id + ": assigning " + username);
           Meteor.users.update(user._id, {$set: {"profile.username": username}});
           return; // process next user
@@ -49,6 +75,11 @@ var setUsernames = function() {
 
 // Migrations.add({
 //   version: version++,
+//   up: cleanUsernames
+// });
+//
+// Migrations.add({
+//   version: version++,
 //   up: unsetDuplicateUsernames
 // });
 //
@@ -57,5 +88,7 @@ var setUsernames = function() {
 //   up: setUsernames
 // });
 
+Meteor.startup(cleanUsernames);
 Meteor.startup(unsetDuplicateUsernames);
 Meteor.startup(setUsernames);
+
