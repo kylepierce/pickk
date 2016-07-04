9b9f5f898f7baaba1c5523ed47446d3287dc33a0
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-07-04 15:11:24 +0300

    Reformat history

--- client/templates/history/history.js
+++ client/templates/history/history.js
@@ -1,23 +1,23 @@
 Template.history.onCreated(function() {
-	this.subscribe('userData');
-	this.subscribe('gamesPlayed');
-	this.autorun(function(computation) {
-		if (Template.instance().subscriptionsReady()) {
-			$(".loader").delay(100).fadeOut('fast', function() {
-				$(".loading-wrapper").fadeIn('fast');
-			});
-			computation.stop()
-		}
-	})
+  this.subscribe('userData');
+  this.subscribe('gamesPlayed');
+  this.autorun(function(computation) {
+    if (Template.instance().subscriptionsReady()) {
+      $(".loader").delay(100).fadeOut('fast', function() {
+        $(".loading-wrapper").fadeIn('fast');
+      });
+      computation.stop()
+    }
+  })
 });
 
-Template.history.onRendered( function() {
-  $( "svg" ).delay( 0 ).fadeIn();
+Template.history.onRendered(function() {
+  $("svg").delay(0).fadeIn();
 });
 
 Template.history.helpers({
-	gamesPlayed: function(){
-		var selector = {users: Meteor.userId()};
-  	return Games.find(selector, {sort: {dateCreated: -1}});
-	}
+  gamesPlayed: function() {
+    var selector = {users: Meteor.userId()};
+    return Games.find(selector, {sort: {dateCreated: -1}});
+  }
 });
