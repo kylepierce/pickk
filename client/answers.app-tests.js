e25e8cb3f5781d37122fa222d8459741a2c1cab7
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-07-07 18:07:25 +0300

    Implement answers tests

--- client/answers.app-tests.js
+++ client/answers.app-tests.js
@@ -13,33 +13,59 @@ if (Meteor.isClient) {
     }
   };
 
-  var waitForSubscriptions = function() {
-    return new Promise(function(resolve, reject) {
-      var poll = Meteor.setInterval(function() {
-        if (DDP._allSubscriptionsReady()) {
-          clearInterval(poll);
-          resolve();
-        }
-      }, 200);
-    });
-  }
+  var waitFor = function(func, timeout, interval) {
+    interval = interval || 200;
+    timeout = timeout || 1000;
+    return function() {
+      return new Promise(function(resolve, reject) {
+        var start = new Date().getTime();
+        var poll = Meteor.setInterval(function() {
+          if (func()) {
+            clearInterval(poll);
+            resolve();
+          } else {
+            var now = new Date().getTime();
+            if (now > start + timeout) {
+              clearInterval(poll);
+              reject(new Error("Timeout"));
+            }
+          }
+        }, interval);
+      });
+    }
+  };
 
-  describe('data available when routed', function() {
-    it('renders the body when routed to', function() {
+  beforeEach(function() {
+    return Promise.resolve()
+      .then(denodeify(function(callback) {return Meteor.call('xolvio:cleaner/resetDatabase', null, callback)}))
+      .then(denodeify(function(callback) {return Meteor.call('pickk/populateDatabase', null, callback)}))
+  });
+
+  describe('questions & answers', function() {
+    it('gives coins for a correct answer', function() {
       Meteor.loginWithToken("CharlieDalton");
       return Promise.resolve()
-        .then(waitForSubscriptions)
+        .then(waitFor(function() {return DDP._allSubscriptionsReady()}))
         .then(denodeify(Tracker.afterFlush))
+        .then(denodeify(function(callback) {return Meteor.call("questionAnswered", "PitchQuestion", "option1", "250", "Strike", callback)}))
+        .then(denodeify(function(callback) {return Meteor.call("deactivateStatus", "PitchQuestion", callback)}))
+        .then(denodeify(function(callback) {return Meteor.call("modifyQuestionStatus", "PitchQuestion", "option1", callback)}))
         .then(function() {
-          Router.go('/');
-        })
-        .then(waitForSubscriptions)
-        .then(denodeify(Tracker.afterFlush))
-        .then(function() {
-          console.log(Meteor.user());
-          console.log($("body").html().indexOf("What is an outcome of the bat?"));
-          assert.equal($('body').length, 1);
+          assert.equal(Meteor.user().profile.coins, 10275);
         });
     })
+
+    it('does not give coins for an incorrect answer', function() {
+      Meteor.loginWithToken("CharlieDalton");
+      return Promise.resolve()
+        .then(waitFor(function() {return DDP._allSubscriptionsReady()}))
+        .then(denodeify(Tracker.afterFlush))
+        .then(denodeify(function(callback) {return Meteor.call("questionAnswered", "PitchQuestion", "option3", "500", "Hit", callback)}))
+        .then(denodeify(function(callback) {return Meteor.call("deactivateStatus", "PitchQuestion", callback)}))
+        .then(denodeify(function(callback) {return Meteor.call("modifyQuestionStatus", "PitchQuestion", "option1", callback)}))
+        .then(function() {
+          assert.equal(Meteor.user().profile.coins, 9500);
+        });
+    });
   })
 }
