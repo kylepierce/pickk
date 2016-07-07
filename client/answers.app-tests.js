5108d0225c24a621a471e4f75fc9a49f320dae75
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-07-07 18:08:36 +0300

    Move common functions into a separate file

--- client/answers.app-tests.js
+++ client/answers.app-tests.js
@@ -1,40 +1,4 @@
 if (Meteor.isClient) {
-  var denodeify = function(func) {
-    return function() {
-      return new Promise(function(resolve, reject) {
-        func(function(error) {
-          if (error) {
-            reject(error)
-          } else {
-            resolve()
-          }
-        })
-      });
-    }
-  };
-
-  var waitFor = function(func, timeout, interval) {
-    interval = interval || 200;
-    timeout = timeout || 1000;
-    return function() {
-      return new Promise(function(resolve, reject) {
-        var start = new Date().getTime();
-        var poll = Meteor.setInterval(function() {
-          if (func()) {
-            clearInterval(poll);
-            resolve();
-          } else {
-            var now = new Date().getTime();
-            if (now > start + timeout) {
-              clearInterval(poll);
-              reject(new Error("Timeout"));
-            }
-          }
-        }, interval);
-      });
-    }
-  };
-
   beforeEach(function() {
     return Promise.resolve()
       .then(denodeify(function(callback) {return Meteor.call('xolvio:cleaner/resetDatabase', null, callback)}))
