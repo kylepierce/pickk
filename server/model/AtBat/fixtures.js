14de90ca29f854e546fc7852ec604e754f747e15
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2016-07-14 19:53:11 +0300

    Add AtBat fixtures.

---
+++ server/model/AtBat/fixtures.js
@@ -0,0 +1,11 @@
+Fixtures.push(AtBat, {
+  AtBat: {
+    dateCreated: moment("2016-05-06").toDate(),
+    createdBy: "CharlieDalton",
+    active: true,
+    playerId: "MartinRamires",
+    gameId: "ActiveGame",
+    ballCount: 1,
+    strikeCount: 0
+  }
+});
