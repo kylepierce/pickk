2526bea16e14576074497b1c4cebe648ad54aa9a
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2016-07-31 23:29:00 -0400

    Moved profile.coins to GamePlayed collection

--- client/templates/leaderboard/gameLeaderboard.js
+++ client/templates/leaderboard/gameLeaderboard.js
@@ -1,11 +1,9 @@
 Template.gameLeaderboard.helpers({
 	'player': function(){
 		var $game = Router.current().params.id
+		// Users change so much this loads the users once. 
 		Fetcher.retrieve("leaderboard", "loadLeaderboard", $game)
 		var leaderboard = Fetcher.get("leaderboard")
-		// var fixed = _.sortBy(leaderboard, function(obj){return obj.profile.coins})
-		// var list = fixed.reverse()
-		// return _.first(list, 25)
 		return GamePlayed.find({gameId: $game}, {sort: {coins: -1}})
 	},
 	'username': function(userId) {
@@ -15,11 +13,5 @@ Template.gameLeaderboard.helpers({
 		// https://github.com/meteoric/meteor-ionic/issues/66
 		var url = "/user-profile/" + this.userId
 		return url
-	}
-});
-
-Template.gameLeaderboard.events({
-	'click .ionItem': function () {
-		// ...
 	}
 });
