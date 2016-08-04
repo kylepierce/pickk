f92c1c694ad4d60edb8818901fb1757d85fcf4c3
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2016-08-04 03:04:09 -0400

    Notification select an option

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -66,14 +66,110 @@ Template.newFollower.helpers({
 	},
 });
 
-Template.notifications.events({
-	'click .item': function (event, template) {
-		console.log(event, template, this)
-		var notification = event.currentTarget
-		Blaze.renderWithData('notificationOptions', notificationContainer)
+Template.notification.events({
+	'click .item': function (e, t) {
+		var displayOptions = function ( o ) {
+			// The select item dom and data
+			var $selected = $(e.currentTarget)
+			var selectedObj = t.data.note
+			var templateName = o.insertedTemplate
+
+			var addOptions = function ( id, data ){
+				var options = "<div id='" + id + "'></div>"
+				$selected.after(options);
+				var container = $('#' + id + '')[0]
+				Blaze.renderWithData(templateName, data, container)
+			}
+
+			var container = $('#' + o.containerId + '')[0]
+			if ( container ){
+				if ( container.previousSibling !== $selected[0] ){
+					container.remove();
+					addOptions( o.containerId, selectedObj )	
+				} else {
+					container.remove();
+				}
+			} else {
+				addOptions( o.containerId, selectedObj )	
+			}
+		}
+		parms = {
+			insertedTemplate: Template.notificationOptions,
+			containerId: "notification-options",
+		 	event: e,
+		 	template: t,
+		}
+		displayOptions( parms )
+	},
+});
+
+Template.notificationOptions.events({
+	'click [data-action=user]': function (event, template) {
+		var note = template.data.data.note
+		console.log(note.senderId)
+	}, 
+	'click [data-action=close]': function(){
+		$('#notification-options').remove();
 	}
 });
 
+Template.notificationOptions.helpers({
+	options: function(){
+		var note = Template.instance().data.type
+		var twoOption = ["score", "diamonds", "badge", "trophy"] 
+		var threeOption = ["follower", "group"]
+		var fourOption = ["mention"]
+
+		numberOfOptions = function (array){
+			if( array.indexOf(note) !== -1 )
+				{return true}
+		}
+
+		if(numberOfOptions(fourOption)){
+			return "col-md-25"
+		} else if (numberOfOptions(threeOption)){
+			return "col-md-33"
+		} else if (numberOfOptions(twoOption)){}
+			return "col-50"
+	},
+	mention: function (template) {
+		var note = Template.instance().data.type
+		if(note === "mention"){
+			return true
+		}
+	},
+	award: function () {
+		var note = Template.instance().data.type
+		var types = ["score", "diamonds", "badge", "trophy"] 
+		if(types.indexOf(note) !== -1){
+			return true
+		}
+	},
+	interaction: function () {
+		var note = Template.instance().data.type
+		var types = ["follower", "mention", "group"] 
+		var exists = types.indexOf(note) !== -1
+		if(exists){
+			return true
+		}
+	},
+	follower: function () {
+		var note = Template.instance().data.type
+		var types = ["follower"] 
+		var exists = types.indexOf(note) !== -1
+		if(exists){
+			return true
+		}		
+	},
+	group: function () {
+		var note = Template.instance().data.type
+		var types = ["group"] 
+		var exists = types.indexOf(note) !== -1
+		if(exists){
+			return true
+		}
+	},
+});
 
 
 
