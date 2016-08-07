ca3656e4b77c0bce061524319167e6be15940983
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2016-08-07 18:40:21 -0400

    Code clean up for sidebar and single game history

--- client/templates/history/history.js
+++ client/templates/history/history.js
@@ -1,20 +1,3 @@
-// Template.history.onCreated(function() {
-//   this.subscribe('userData');
-//   this.subscribe('gamesPlayed');
-//   this.autorun(function(computation) {
-//     if (Template.instance().subscriptionsReady()) {
-//       $(".loader").delay(100).fadeOut('fast', function() {
-//         $(".loading-wrapper").fadeIn('fast');
-//       });
-//       computation.stop()
-//     }
-//   })
-// });
-
-// Template.history.onRendered(function() {
-//   $("svg").delay(0).fadeIn();
-// });
-
 Template.history.helpers({
   gamesPlayed: function() {
     var selector = {users: Meteor.userId()};
