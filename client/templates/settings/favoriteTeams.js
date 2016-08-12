54020176c77f0d3fb03a7deb23761433b2938389
Niranjan Singh <niranjan987@gmail.com>
2016-08-11 18:43:17 -0700

    Fav sports and multiple teams auto form

--- client/templates/settings/favoriteTeams.js
+++ client/templates/settings/favoriteTeams.js
@@ -1,41 +1,73 @@
 SimpleSchema.debug = true;
 
-AutoForm.hooks({
-  favoriteTeamsForm: {
-    onSubmit: function (insertDoc, updateDoc, currentDoc) {
-      this.event.preventDefault();
-      var done = this.done;
+var currentStep = new ReactiveVar('');
+var stepsToDo = [];   // Start with sports selection, and then add selected teams 
 
-      var favTeamsArr = updateDoc['$set'].favoriteTeams;
+var hooksObj = {
+  onSubmit: function (insertDoc, updateDoc, currentDoc) {
+    this.event.preventDefault();
+    var done = this.done;
 
-      Meteor.call('updateFavoriteTeams', favTeamsArr, function(error) {
-        if (error) {
-          done(error);
-        } else {
-          
-          if (Meteor.isCordova && Meteor.user().OneSignalToken) {
-            // Update OneSignal Tags
-            updateOneSignalTeamTags(Meteor.user());
-          }
-
-          done();
-          if (Meteor.user().profile.isOnboarded) {
-            Router.go('/');
-          } else {
-            Router.go('/onboarding');
-          }
-        }
-      });
-
-      done();
+    if (!updateDoc['$set']) {
+      done(new Error('Invalid selection'));
       return false;
     }
+
+    var selectionArray = updateDoc['$set'][currentStep.get()];
+
+    if (currentStep.get() === 'favoriteSports') {
+      stepsToDo = selectionArray;   // Used to move to next step
+    }
+
+    Meteor.call('updateFavorites', selectionArray, currentStep.get(), function(error) {
+      if (error) {
+        done(error);
+      } else {
+        if (Meteor.isCordova && Meteor.user().OneSignalToken) {
+          // Update OneSignal Tags
+          updateOneSignalTeamTags(Meteor.user());
+        }
+        done();
+        moveNextStep();
+      }
+    });
+    done();
+    return false;
   }
+};
+
+AutoForm.addHooks(['favoriteSports', 'favoriteMLBTeams', 'favoriteNFLTeams'], hooksObj);
+
+Template.favoriteTeams.onCreated(function (){
+  this.allSteps = [ 'favoriteSports', 'favoriteMLBTeams', 'favoriteNFLTeams'];
+  currentStep = new ReactiveVar('favoriteSports');
 });
 
 
 Template.favoriteTeams.helpers({
+  allSteps: function () {
+    return Template.instance().allSteps;
+  },
   UsersProfileSchema: function() {
     return UsersProfileSchema;
+  },
+  hide: function (step) {
+    if (step !== currentStep.get()) {
+      return 'hidden';
+    }
   }
 });
+
+var moveNextStep = function () {
+  var step = stepsToDo.pop();
+  if (step) {
+    currentStep.set('favorite' + step + 'Teams'); 
+  } else {
+    // End of steps
+    if (Meteor.user().profile.isOnboarded) {
+      Router.go('/');
+    } else {
+      Router.go('/onboarding');
+    }
+  }
+}
