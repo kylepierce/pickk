101e7bd0f97775d5edead4074556979804a9ea32
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2016-09-12 20:09:03 +0300

    Add modified Intercom version into the project.

---
+++ client/intercom.js
@@ -0,0 +1,9 @@
+IntercomSettings.userInfo = function (user, info) {
+  if (!user.intercomHash) {
+    return false; // the hash isn't loaded to the users collection yet. come back later.
+  }
+
+  info.name = user.profile.firstName + " " + user.profile.lastName;
+
+  return true;
+};
