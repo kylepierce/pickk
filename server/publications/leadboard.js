f213c1b8838eab57eff44910e693d29b6940b14c
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2016-10-26 08:12:58 -0400

    Used similar url/limit structure for leaderboard and history

--- server/publications/leadboard.js
+++ server/publications/leadboard.js
@@ -16,17 +16,18 @@ Meteor.publish('weekLeaderboard', function(thisWeek){
 	return UserList.find({_id: {$in: newArray}}, {fields: {_id: 1, avatar: 1, services: 1, 'profile.username': 1}})
 });
 
-Meteor.publish('leaderboardGamePlayed', function(game, count) {
+Meteor.publish('leaderboardGamePlayed', function(game, number) {
   check(game, String);
   this.unblock()
   
   var selector = {gameId: game}
   var fields = {fields: {userId: 1, gameId: 1, coins: 1}}
   
-  if(count === "all"){
-  	var sort = {sort: {coins: -1}}
+  if (number === -1){
+    var sort = {sort: {dateCreated: -1}}
   } else {
-  	var sort = {sort: {coins: -1}, limit: count}
+    var sort = {sort: {dateCreated: -1}, limit: number}
   }
+  
   return GamePlayed.find(selector, fields, sort)
 });
