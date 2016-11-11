824ce605d570eda241d6ef8caba2d5eba30823d3
Kyle Pierce <kyle.pierce@huskers.unl.edu>
2016-11-11 18:25:09 -0500

    Make sure props are valid before adding them

--- client/templates/admin/_propQuestion.js
+++ client/templates/admin/_propQuestion.js
@@ -14,16 +14,14 @@ Template._propQuestion.helpers({
 
 Template._propQuestion.events({
 	'change .option-range': function(e, t){
-		console.log(e, t)
 		var isNotANumber = isNaN(e.currentTarget.value) 
 		console.log(isNotANumber)
 		if (isNotANumber === true){
-			console.log("Alert")
+			alert("That is not a number");
 		} else if (isNotANumber === false){
 			var box = e.currentTarget.classList
 			console.log(box)
 			box[1] = 'acceptable'
-			
 		}
 	},
 	'click [data-action=teamOne]': function () {
@@ -52,15 +50,37 @@ Template._propQuestion.events({
 		var question = $('#question-input').val()
 		var options = {}
 
+		if (!question){
+			inputError("Missing A Question! C'Mon Man")
+			return false
+		}
+
 		function randomizer(min, max){
 			var min = parseFloat(min)
-			var max = parseFloat(max)
+			if (!max){
+				var max = parseFloat(min) 
+			} else {
+				var max = parseFloat(max)
+			}
 			var multi = (Math.random() * (max-min) + min).toFixed(1)
 			return multi
 		}
 
+		function inputError(reason){
+			console.log(reason)
+			sAlert.error("Error: " + reason , {effect: 'slide', position: 'bottom', html: true});
+		}
+
 		// How many option boxes have been filled?
 		var optionBoxes = $('.option-boxes')
+		if (optionBoxes[0].children[0].value === ""){
+			inputError("No options");
+			return false
+		} else if ( optionBoxes[1].children[0].value === "") {
+			inputError("Not Enough Options. At least 2 Options Are Needed!");
+			return false
+		}
+
 		for (i = 0; i < optionBoxes.length; i++) {
 			var title = optionBoxes[i].children[0].value
 			
@@ -71,6 +91,20 @@ Template._propQuestion.events({
 				var low = optionBoxes[i].children[1].value
 				var high = optionBoxes[i].children[2].value
 				var multi = randomizer(low, high)
+				var isNotANumber = isNaN(multi)
+				console.log(low, high, multi, isNotANumber)
+				if (!low) {
+					inputError("Missing Multipliers!")
+					return false
+				}
+				if (low > high){
+					inputError("Min value cannot be larger than the max!")
+					return false
+				} 
+				if (isNotANumber) {
+					inputError("Not a Number / Missing Multiplier!")
+					return false
+				}
 
 				options[optionNum] = {
 					number: i + 1,
@@ -79,8 +113,8 @@ Template._propQuestion.events({
 					multiplier: multi
 				}
 			}
-			
 		}
+		console.log(options)
 		var q = {
 			que: question,
 			gameId: gameId,
@@ -91,5 +125,6 @@ Template._propQuestion.events({
 		}
 
 		Meteor.call('createProp', q);
+		sAlert.success("Posted " + question + "!" , {effect: 'slide', position: 'bottom', html: true});
 	}
 });
