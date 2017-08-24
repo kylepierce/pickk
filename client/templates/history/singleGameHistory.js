bd48c9c6ba868b7a9b651ede60492fc9cf0b3665
pickk <hi@pickk.co>
2017-08-24 03:32:42 -0400

    Finished question history list

--- client/templates/history/singleGameHistory.js
+++ client/templates/history/singleGameHistory.js
@@ -35,7 +35,7 @@ Template.singleAnswer.helpers({
 		if (contains > -1) {
 			return "history-correct" // Correct
 		} else if (this.q.active === true){
-			return "history-inprogress" // In progress
+			return "history-live" // In progress
 		} else if (this.q.active === null){
 			return "history-pending" // Pending
 		} else {
@@ -73,14 +73,16 @@ Template.singleAnswer.helpers({
 	winnings: function ( answered, outcome ){
 		var contains = outcome.indexOf(answered)
 
+		addCommas = function(number){
+			return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		}
+
 		if (contains > -1) {
-			return "+" + parseInt(this.a.wager * this.a.multiplier)
+			return "+ " + addCommas(parseInt(this.a.wager * this.a.multiplier)) + "</strong>"
 		} else if (outcome === "Removed" || this.q.type === 'freePickk'){
-			return "+" + this.a.wager
+			return "+ " + this.a.wager
 		} else if (outcome !== undefined && answered !== outcome) {
-			return "-" + this.a.wager
-		} else {
-			return "Waiting..."
+			return "- " + this.a.wager
 		}
 	},
 	details: function() {
@@ -88,5 +90,30 @@ Template.singleAnswer.helpers({
 		if (extended){
 			return true
 		}
+	},
+	timeAgo: function(){
+		var answerCreated = this.a.dateCreated
+		var now = new Date;
+		var diff = now - answerCreated
+
+		var timeConverter = function(time, length) {
+			var timeNumber = time / length
+			return parseFloat(timeNumber).toFixed(0)
+		}
+
+		if (diff < 60000) {
+			var timeNumber = timeConverter(diff, 1000)
+			return timeNumber + " Seconds Ago"
+		} else if (diff > 60000 && diff < 3600000) {
+			var timeNumber = timeConverter(diff, 60000)
+			return timeNumber + " Mins Ago"
+		} else if (diff > 3600000 && diff < 86400000) {
+			var timeNumber = timeConverter(diff, 3600000)
+			return timeNumber + " Hours Ago"
+		} else if (diff > 86400000){
+			var timeNumber = timeConverter(diff, 86400000)
+			return timeNumber + " Days Ago"
+		}
+		console.log(now, answerCreated, diff);
 	}
 });
