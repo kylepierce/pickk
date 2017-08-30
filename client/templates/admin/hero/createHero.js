a0f50193b125b87def196372fd764af3ff05973d
pickk <hi@pickk.co>
2017-08-30 13:57:41 -0400

    Added additional hero fields

--- client/templates/admin/hero/createHero.js
+++ client/templates/admin/hero/createHero.js
@@ -1,6 +1,6 @@
 AutoForm.addHooks(['createHero'], {
   onSuccess: function(operation, result, template) {
-    console.log(operation, result, template);
+    // console.log(operation, result, template);
     this.event.preventDefault();
     // Router.go("/groups/association/" + result);
   }
