34ac83bc69a66cec90ad489f37424fc9050dadb9
pickk <hi@pickk.co>
2017-09-04 19:43:10 -0400

    Figure out how the auto form system works and updated the privacy and # of users

---
+++ client/templates/core/autoform-template.js
@@ -0,0 +1,92 @@
+Template['afCheckboxGroup_ionic2'].events({
+  'click .checkbox': function(e,t){
+    var input = $("input[id='"+ this.value+"']")[0];
+    $(e.currentTarget).toggleClass('play-selected');
+  }
+});
+
+Template["afCheckboxGroup_ionic2"].helpers({
+  img: function(){
+    var lower = this.value.toLowerCase()
+    var image = "/team-logos/" + lower + ".svg"
+    return image
+  },
+  selected: function(){
+    if (this.selected) {
+      return "play-selected"
+    }
+  },
+  atts: function selectedAttsAdjust() {
+    var atts = _.clone(this.atts);
+    if (this.selected) {
+      atts.checked = "";
+    }
+    // remove data-schema-key attribute because we put it
+    // on the entire group
+    delete atts["data-schema-key"];
+    return atts;
+  },
+  dsk: function dsk() {
+    return {
+      "data-schema-key": this.atts["data-schema-key"]
+    }
+  }
+});
+
+Template['afRadioGroup_ionic2'].events({
+  'click .button-card': function(e,t){
+    $(e.currentTarget.previousElementSibling)[0].checked = true
+  }
+});
+
+Template["afRadioGroup_ionic2"].helpers({
+  img: function(){
+    var lower = this.value.toLowerCase()
+    var image = "/icons/" + lower + ".svg"
+    return image
+  },
+  atts: function selectedAttsAdjust() {
+    var atts = _.clone(this.atts);
+    if (this.selected) {
+      atts.checked = "";
+    }
+    // remove data-schema-key attribute because we put it
+    // on the entire group
+    delete atts["data-schema-key"];
+    return atts;
+  },
+  dsk: function dsk() {
+    return {
+      "data-schema-key": this.atts["data-schema-key"]
+    }
+  }
+});
+
+Template['afRadioGroup_small'].events({
+  'click .button-card': function(e,t){
+    $(e.currentTarget.previousElementSibling)[0].checked = true
+  }
+});
+
+Template["afRadioGroup_small"].helpers({
+  img: function(){
+    var lower = this.value.toLowerCase()
+    var image = "/team-logos/" + lower + ".svg"
+    return image
+  },
+  atts: function selectedAttsAdjust() {
+    var atts = _.clone(this.atts);
+    if (this.selected) {
+      atts.checked = "";
+    }
+    // remove data-schema-key attribute because we put it
+    // on the entire group
+    delete atts["data-schema-key"];
+    return atts;
+  },
+  dsk: function dsk() {
+    return {
+      "data-schema-key": this.atts["data-schema-key"]
+    }
+  }
+});
--- client/templates/leagues/_createLeague.js
+++ client/templates/leagues/_createLeague.js
@@ -1,6 +1,7 @@
 AutoForm.addHooks(['createLeague'], {
   onSuccess: function(operation, result, template) {
+    console.log(operation, result, template);
     this.event.preventDefault();
-    Router.go("/league/settings/photo/" + result);
+    // Router.go("/league/settings/photo/" + result);
   }
 });
