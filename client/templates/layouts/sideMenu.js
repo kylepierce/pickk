9f9bceb05d6e57756598de25d02b7e6a50ebe881
pickk <hi@pickk.co>
2017-09-04 04:48:07 -0400

    Custom svg icons and array system for sidebar

---
+++ client/templates/layouts/sideMenu.js
@@ -0,0 +1,15 @@
+Template.sideMenuContent.helpers({
+  links: function(){
+    return [
+      {title: "Home", path: "home", icon: "home"},
+      {title: "Prizes", path: "prizes", icon: "prizes"},
+      {title: "Game Schedule", path: "games", icon: "calendar"},
+      {title: "Matchups", path: "matchups", icon: "stadium"},
+      {title: "Leagues", path: "leagues", icon: "lockers"},
+      {title: "Week Leaderboard", path: "week-leaderboard", icon: "podium"},
+      {title: "Common Questions", path: "rules", icon: "faq"},
+      {title: "Game History", path: "history", icon: "scoreboard"},
+      {title: "Settings", path: "settings", icon: "settings"},
+    ]
+  }
+});
